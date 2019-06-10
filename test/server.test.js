22cd802efec16f5579ece260dc8138d1e7bb6ee5
Walid S Newaz <mdwalid@gmail.com>
2019-06-10 01:06:28 -0600

    Added React, NextJS

---
+++ test/server.test.js
@@ -0,0 +1,10 @@
+describe('Server Test', () => {
+    describe('Empty test', () => {
+        it('just an empty test', () => {
+            
+        });
+        it('This is another empty test', () => {
+            console.log('This has no meaning...');
+        });
+    });
+});
